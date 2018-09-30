a390e7054cefef28f6b615fdfad514745e6a349c
HuuBaa <742790905@qq.com>
2018-09-30 21:50:52 +0800

    fix

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
-basedir=os.path.abspath(os.path.dirname(__file__))
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
-    SECRET_KEY='HUU IS COOL'
+    SECRET_KEY = 'HUU IS COOL'
     FLASK_MAIL_SUBJECT_PREFIX = '[任务村]'
     FLASK_MAIL_SENDER = '任务村 <742790905@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
@@ -14,21 +16,24 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    BOOTSTRAP_SERVE_LOCAL=True
+    BOOTSTRAP_SERVE_LOCAL = True
+
 
 class DevelopmentConfig(Config):
-    DEBUG=False
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-development.sqlite')
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-production.sqlite')
 
+
 class TestConfig(Config):
-    TESTING=True
+    TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
-config={'default':DevelopmentConfig,
-        'development':DevelopmentConfig,
-        'production':ProductionConfig,
-        'test':TestConfig}
+
+config = {'default': DevelopmentConfig,
+          'development': DevelopmentConfig,
+          'production': ProductionConfig,
+          'test': TestConfig}
